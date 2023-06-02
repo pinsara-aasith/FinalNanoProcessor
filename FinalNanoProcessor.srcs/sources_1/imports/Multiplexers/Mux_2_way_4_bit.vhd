22c6a345ff4fbadc61f4babcfdf10305cc3d8ed8
pinsara.ecyber <pinsara@ecyber.com>
2023-06-03 01:25:59 +0530

    add 2 way 4 bit mux back

---
+++ FinalNanoProcessor.srcs/sources_1/imports/Multiplexers/Mux_2_way_4_bit.vhd
@@ -0,0 +1,29 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+
+ENTITY Mux_2_to_4 IS
+    PORT (
+        S : IN STD_LOGIC;
+        D_0 : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
+        D_1 : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
+        Y : OUT STD_LOGIC_VECTOR(3 DOWNTO 0)
+    );
+END Mux_2_to_4;
+
+ARCHITECTURE Behavioral OF Mux_2_to_4 IS
+
+BEGIN
+
+    PROCESS (S, D_0, D_1)
+    BEGIN
+        CASE S IS
+            WHEN '0' =>
+                Y <= D_0;
+            WHEN '1' =>
+                Y <= D_1;
+            WHEN OTHERS =>
+                Y <= (OTHERS => 'X');
+        END CASE;
+    END PROCESS;
+
+END Behavioral;
