5d86f68c2c09d6c4e9299d26ff57b701c9cc5572
pinsara.ecyber <pinsara@ecyber.com>
2023-06-11 23:00:57 +0530

    rom change & more tests

--- FinalNanoProcessor.srcs/sim_1/new/TB_Instruction_decoder.vhd
+++ FinalNanoProcessor.srcs/sim_1/new/TB_Instruction_decoder.vhd
@@ -35,8 +35,8 @@ ARCHITECTURE Behavioral OF tb_Instruction_decoder IS
 
 BEGIN
     -- Instantiate the DUT
-    UUT: Instruction_decoder
-    PORT MAP (
+    UUT : Instruction_decoder
+    PORT MAP(
         Instruction => Instruction,
         Reg_en => Reg_en,
         Load_sel => Load_sel,
@@ -52,74 +52,37 @@ BEGIN
     -- Clock process
     PROCESS
     BEGIN
-        WAIT FOR 10 ns;
-        WAIT FOR 10 ns;
-        WAIT FOR 10 ns;
+        WAIT FOR 10ns;
+        WAIT FOR 10ns;
+        WAIT FOR 10ns;
         WAIT;
     END PROCESS;
-
-
     PROCESS
     BEGIN
-        -- Initialize inputs
-        Instruction <= "000000000000";
         Zero_flag <= '0';
 
-        -- Test 1: ADD instruction
-        Instruction <= "000000001100";
-        wait for 10 ns;
+        Instruction <= "100010000001"; -- MOVI  R1, 1 
+        WAIT FOR 20ns;
 
-        -- Test 2: NEG instruction
-        Instruction <= "010101110000";
-        wait for 10 ns;
+        Instruction <= "101110000000"; -- MOVI  R7, 0
+        WAIT FOR 20ns;
 
-        -- Test 3: MOV instruction
-        Instruction <= "101001001010";
-        wait for 10 ns;
+        Instruction <= "001110010000"; -- ADD   R7, R1
+        WAIT FOR 20ns;
 
-        -- Test 4: JZR instruction (Zero flag = 0)
-        Instruction <= "110010110001";
+        Instruction <= "011110000000"; -- NEG   R7
+        WAIT FOR 20ns;
+
         Zero_flag <= '0';
-        wait for 10 ns;
+        Instruction <= "110010000001"; -- JZR   R1, 1
+        WAIT FOR 20ns;
 
-        -- Test 5: JZR instruction (Zero flag = 1)
-        Instruction <= "110010110001";
         Zero_flag <= '1';
-        wait for 10 ns;
+        Instruction <= "110010000001";
+        WAIT FOR 20ns;
 
-        wait;
+        WAIT;
+
     END PROCESS;
 
 END Behavioral;
-
--- Instantiate the testbench
-ARCHITECTURE testbench_arch OF tb_Instruction_decoder IS
-    COMPONENT Instruction_decoder
-        PORT (
-            Instruction : IN STD_LOGIC_VECTOR(11 DOWNTO 0);
-            Reg_en : OUT STD_LOGIC_VECTOR(2 DOWNTO 0);
-            Load_sel : OUT STD_LOGIC;
-            Imd_val : OUT STD_LOGIC_VECTOR(3 DOWNTO 0);
-            Reg_select_A : OUT STD_LOGIC_VECTOR(2 DOWNTO 0);
-            Reg_select_B : OUT STD_LOGIC_VECTOR(2 DOWNTO 0);
-            Add_sub_sel : OUT STD_LOGIC;
-            Jump : OUT STD_LOGIC;
-            Jump_address : OUT STD_LOGIC_VECTOR(2 DOWNTO 0);
-            Zero_flag : IN STD_LOGIC
-        );
-    END COMPONENT;
-BEGIN
-    UUT: Instruction_decoder
-    PORT MAP (
-        Instruction => Instruction,
-        Reg_en => Reg_en,
-        Load_sel => Load_sel,
-        Imd_val => Imd_val,
-        Reg_select_A => Reg_select_A,
-        Reg_select_B => Reg_select_B,
-        Add_sub_sel => Add_sub_sel,
-        Jump => Jump,
-        Jump_address => Jump_address,
-        Zero_flag => Zero_flag
-    );
-END testbench_arch;
--- FinalNanoProcessor.srcs/sources_1/new/ROM.vhd
+++ FinalNanoProcessor.srcs/sources_1/new/ROM.vhd
@@ -10,16 +10,17 @@ ENTITY ROM IS
 END ROM;
 
 ARCHITECTURE Behavioral OF ROM IS
-    TYPE rom_type IS ARRAY (0 TO 5) OF STD_LOGIC_VECTOR(11 DOWNTO 0);
+    TYPE rom_type IS ARRAY (0 TO 7) OF STD_LOGIC_VECTOR(11 DOWNTO 0);
 
     SIGNAL rom : rom_type := (
-        "100010001010", -- MOVI R1, 10 ; R1 ? 10                 format: 10 RRR 000 dddd
-        "100100000001", -- MOVI R2, 1  ; R2 ? 1                  format: 10 RRR 000 dddd
-        "010100000000", -- NEG R2      ; R2 ? -R2                format: 01 RRR 0000000
-        "000010100000", -- ADD R1, R2  ; R1 ? R1 + R2            format: 00 RaRaRa RbRbRb 0000
-        "110010000110", -- JZR R1, 7   ; If R1 = 0 jump to line 7 format: 11 RRR 0000 ddd
-        "110000000011" -- JZR R0, 3   ; If R0 = 0 jump to line 3 format: 11 RRR 0000 ddd
-        -- no line 7
+        "100100000011", -- MOVI     R2, 3
+        "101110000000", -- MOVI     R7, 0
+        "101000001111", -- MOVI     R4, -1
+        "001110100000", -- ADD      R7, R2 
+        "000101000000", -- ADD      R2, R4
+        "110100000111", -- JZR      R2, 8
+        "110000000011", -- JZR      R0,4
+        "001110000000" --  ADD      R7, R0
     );
 
 BEGIN
