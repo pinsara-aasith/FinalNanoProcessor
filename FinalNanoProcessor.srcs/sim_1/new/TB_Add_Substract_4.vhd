6f8a122bceb6c9379ec0c0a5c280d355d10122a7
pinsara.ecyber <pinsara@ecyber.com>
2023-06-11 22:15:58 +0530

    test add sub unit

--- FinalNanoProcessor.srcs/sim_1/new/TB_Add_Substract_4.vhd
+++ FinalNanoProcessor.srcs/sim_1/new/TB_Add_Substract_4.vhd
@@ -14,9 +14,7 @@ ARCHITECTURE behaviour OF Add_Substract_4_TB IS
          AddSub_Ctrl : IN STD_LOGIC;
          Zero_flag : OUT STD_LOGIC;
          Carry_flag : OUT STD_LOGIC;
-         Overflow_flag : OUT STD_LOGIC;
          Sign_flag : OUT STD_LOGIC;
-         Parity_flag : OUT STD_LOGIC
       );
    END COMPONENT;
 
@@ -29,61 +27,64 @@ ARCHITECTURE behaviour OF Add_Substract_4_TB IS
    SIGNAL Result : STD_LOGIC_VECTOR(3 DOWNTO 0);
    SIGNAL Zero_flag : STD_LOGIC;
    SIGNAL Carry_flag : STD_LOGIC;
-   SIGNAL Overflow_flag : STD_LOGIC;
    SIGNAL Sign_flag : STD_LOGIC;
-   SIGNAL Parity_flag : STD_LOGIC;
 
 BEGIN
+
+A : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
+B : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
+Result : OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
+
+AddSub_Ctrl : IN STD_LOGIC;
+
+Zero_flag : OUT STD_LOGIC;
+Carry_flag : OUT STD_LOGIC;
+Sign_flag : OUT STD_LOGIC
 
    UUT : Add_Substract_4 PORT MAP(
       A => A,
       B => B,
       Result => Result,
       AddSub_Ctrl => AddSub_Ctrl,
+
       Zero_flag => Zero_flag,
       Carry_flag => Carry_flag,
-      Overflow_flag => Overflow_flag,
       Sign_flag => Sign_flag,
-      Parity_flag => Parity_flag
    );
 
    PROCESS
    BEGIN
+
+      --INDEX NO => 11 0011 0101 1011 1010
       -- Test 1: Addition
-      A <= "0010";
-      B <= "0100";
+      A <= "1010";
+      B <= "1011";
       AddSub_Ctrl <= '0';
-      WAIT FOR 10 ns;
+      WAIT FOR 100ns;
 
       -- Test 2: Subtraction
-      A <= "0110";
-      B <= "0010";
+      A <= "1011";
+      B <= "1010";
       AddSub_Ctrl <= '1';
-      WAIT FOR 10 ns;
+      WAIT FOR 100ns;
 
-      -- Test 3: Addition with carry
-      A <= "1101";
-      B <= "0111";
-      AddSub_Ctrl <= '0';
-      WAIT FOR 10 ns;
-
-      -- Test 4: Subtraction with borrow
-      A <= "1001";
-      B <= "1110";
-      AddSub_Ctrl <= '1';
-      WAIT FOR 10 ns;
-
-      -- Test 5: Addition with zero result
+      -- Test 3: Addition with zero result
       A <= "0000";
       B <= "0000";
       AddSub_Ctrl <= '0';
-      WAIT FOR 10 ns;
+      WAIT FOR 100ns;
 
-      -- Test 6: Subtraction with zero result
-      A <= "1010";
-      B <= "1010";
+      -- Test 4: Substraction with negative result
+      A <= "0000";
+      B <= "0001";
       AddSub_Ctrl <= '1';
-      WAIT FOR 10 ns;
+      WAIT FOR 100ns;
+
+      -- Test 5: Addition with carry
+      A <= "1111";
+      B <= "0001";
+      AddSub_Ctrl <= '0';
+      WAIT FOR 100ns;
 
       WAIT;
    END PROCESS;
