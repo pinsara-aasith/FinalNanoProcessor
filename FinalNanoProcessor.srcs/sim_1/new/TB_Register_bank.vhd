94dcb369565fc23c22aa3e4929e67d87bd56baca
pinsara.ecyber <pinsara@ecyber.com>
2023-06-11 22:15:50 +0530

    test reg bank

--- FinalNanoProcessor.srcs/sim_1/new/TB_Register_bank.vhd
+++ FinalNanoProcessor.srcs/sim_1/new/TB_Register_bank.vhd
@@ -1,32 +1,30 @@
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 
-ENTITY tb_Register_bank IS
-END tb_Register_bank;
+ENTITY TB_Reg_Bank IS
+END TB_Reg_Bank;
 
-ARCHITECTURE Behavioral OF tb_Register_bank IS
-    -- Component declaration for the DUT (Design Under Test)
-    COMPONENT Register_bank
-        PORT (
-            Reg_En : IN STD_LOGIC_VECTOR (2 DOWNTO 0);
-            A : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
-            Clk : IN STD_LOGIC;
-            Reset_Register_bank : IN STD_LOGIC;
-            B0 : OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
-            B1 : OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
-            B2 : OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
-            B3 : OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
-            B4 : OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
-            B5 : OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
-            B6 : OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
-            B7 : OUT STD_LOGIC_VECTOR (3 DOWNTO 0)
+ARCHITECTURE Behavioral OF TB_Reg_Bank IS
+    COMPONENT Register_Bank PORT (
+        Reg_En : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+        Clk : IN STD_LOGIC;
+        A : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+        Reg_Bank_En : IN STD_LOGIC;
+        Reset_Register_bank : IN STD_LOGIC;
+        B0 : OUT STD_LOGIC_VECTOR(3 DOWNTO 0);
+        B1 : OUT STD_LOGIC_VECTOR(3 DOWNTO 0);
+        B2 : OUT STD_LOGIC_VECTOR(3 DOWNTO 0);
+        B3 : OUT STD_LOGIC_VECTOR(3 DOWNTO 0);
+        B4 : OUT STD_LOGIC_VECTOR(3 DOWNTO 0);
+        B5 : OUT STD_LOGIC_VECTOR(3 DOWNTO 0);
+        B6 : OUT STD_LOGIC_VECTOR(3 DOWNTO 0);
+        B7 : OUT STD_LOGIC_VECTOR(3 DOWNTO 0)
         );
     END COMPONENT;
 
-    -- Signal declarations
     SIGNAL Reg_En : STD_LOGIC_VECTOR(2 DOWNTO 0);
+    SIGNAL Clk : STD_LOGIC := '0';
     SIGNAL A : STD_LOGIC_VECTOR(3 DOWNTO 0);
-    SIGNAL Clk : STD_LOGIC;
     SIGNAL Reset_Register_bank : STD_LOGIC;
     SIGNAL B0 : STD_LOGIC_VECTOR(3 DOWNTO 0);
     SIGNAL B1 : STD_LOGIC_VECTOR(3 DOWNTO 0);
@@ -37,12 +35,13 @@ ARCHITECTURE Behavioral OF tb_Register_bank IS
     SIGNAL B6 : STD_LOGIC_VECTOR(3 DOWNTO 0);
     SIGNAL B7 : STD_LOGIC_VECTOR(3 DOWNTO 0);
 
+    CONSTANT CLK_CYCLE : TIME := 10ns;
+
 BEGIN
-    UUT: Register_bank
-    PORT MAP (
+    UUT : Register_Bank PORT MAP(
         Reg_En => Reg_En,
-        A => A,
         Clk => Clk,
+        A => A,
         Reset_Register_bank => Reset_Register_bank,
         B0 => B0,
         B1 => B1,
@@ -53,77 +52,47 @@ BEGIN
         B6 => B6,
         B7 => B7
     );
+    Clk <= NOT Clk AFTER CLK_CYCLE / 2;
 
-    -- Clock process
-    clk_proc: PROCESS
+    PROCESS
+        --INDEX NO => 11 0011 0101 1011 1010
     BEGIN
-        Clk <= '0';
-        wait for 5 ns;
-        Clk <= '1';
-        wait for 5 ns;
-    END PROCESS clk_proc;
-
-
-    sim_process: PROCESS
-    BEGIN
-        -- Initialize inputs
-        Reg_En <= "000";
-        A <= "0000";
+        Reg_En <= "001";
+        A <= "1010";
         Reset_Register_bank <= '0';
-        wait for 10 ns;
-        
-        -- Enable registers 1, 3, 5
-        Reg_En <= "101";
-        A <= "0101";
-        wait for 10 ns;
-        
-        -- Disable registers 1, 3, 5 and enable registers 0, 2, 4, 6, 7
-        Reg_En <= "111";
-        A <= "0011";
-        wait for 10 ns;
-        
-        -- Reset the register bank
+        WAIT FOR CLK_CYCLE;
+
+        Reg_En <= "001";
+        A <= "1011";
         Reset_Register_bank <= '1';
-        wait for 10 ns;
+        WAIT FOR CLK_CYCLE;
+
+        Reg_En <= "010";
+        A <= "0101";
         Reset_Register_bank <= '0';
+        WAIT FOR CLK_CYCLE;
+
+        Reg_En <= "011";
+        A <= "0011";
+        Reset_Register_bank <= '0';
+        WAIT FOR CLK_CYCLE;
+
+        Reg_En <= "100";
+        A <= "1010";
+        Reset_Register_bank <= '0';
+        WAIT FOR CLK_CYCLE;
+
+        Reg_En <= "110";
+        A <= "1010";
+        Reset_Register_bank <= '0';
+        WAIT FOR CLK_CYCLE;
         
-        wait;
-    END PROCESS sim_process;
+        Reg_En <= "111";
+        A <= "1010";
+        Reset_Register_bank <= '0';
+        WAIT FOR CLK_CYCLE;
 
+        Reset_Register_bank <= '1';
+        WAIT FOR CLK_CYCLE;
+    END PROCESS;
 END Behavioral;
-
--- Instantiate the testbench
-ARCHITECTURE testbench_arch OF tb_Register_bank IS
-    COMPONENT Register_bank
-        PORT (
-            Reg_En : IN STD_LOGIC_VECTOR (2 DOWNTO 0);
-            A : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
-            Clk : IN STD_LOGIC;
-            Reset_Register_bank : IN STD_LOGIC;
-            B0 : OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
-            B1 : OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
-            B2 : OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
-            B3 : OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
-            B4 : OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
-            B5 : OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
-            B6 : OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
-            B7 : OUT STD_LOGIC_VECTOR (3 DOWNTO 0)
-        );
-    END COMPONENT;
-BEGIN
-    UUT: Register_bank
-    PORT MAP (
-        Reg_En => Reg_En,
-        A => A,
-        Clk => Clk,
-        Reset_Register_bank => Reset_Register_bank,
-        B0 => B0,
-        B1 => B1,
-        B2 => B2,
-        B3 => B3,
-        B4 => B4,
-        B5 => B5,
-        B6 => B6,
-        B7 => B7
-    );
-END testbench_arch;
